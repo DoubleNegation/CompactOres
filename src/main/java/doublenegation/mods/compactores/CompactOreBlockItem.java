ea78f157115e3d00e67af5015f593cb3671ae5ea
DoubleNegation <43982164+DoubleNegation@users.noreply.github.com>
2020-02-08 11:42:49 +0100

    Fix a crash on the dedicated server when a block name is printed to chat
    Happens for example when using the /give command to obtain a compact ore.

--- src/main/java/doublenegation/mods/compactores/CompactOreBlockItem.java
+++ src/main/java/doublenegation/mods/compactores/CompactOreBlockItem.java
@@ -5,6 +5,11 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.registries.GameData;
+
+import java.util.Map;
 
 public class CompactOreBlockItem extends BlockItem {
 
@@ -18,7 +23,25 @@ public class CompactOreBlockItem extends BlockItem {
 
     @Override
     public ITextComponent getDisplayName(ItemStack stack) {
-        return block.getNameTextComponent();
+        return DistExecutor.runForDist(() -> () -> {
+            // CLIENT
+            return block.getNameTextComponent();
+        }, () -> () -> {
+            // SERVER
+            // Block#getNameTextComponent() is not a thing on the dedicated server, so don't use it here.
+            // Language files apparently also aren't, so a TranslationTextComponent won't do much good either.
+            if(block instanceof CompactOreBlock) {
+                Map<Block, Item> blockItemMap = GameData.getBlockItemMap();
+                Block baseBlock = ((CompactOreBlock) block).baseBlock();
+                if(blockItemMap.containsKey(baseBlock)) {
+                    ITextComponent baseName = blockItemMap.get(baseBlock).getDisplayName(stack);
+                    StringTextComponent name = new StringTextComponent("Compact ");
+                    name.appendSibling(baseName);
+                    return name;
+                }
+            }
+            return new StringTextComponent("Compact Ore");
+        });
     }
 
     @Override
