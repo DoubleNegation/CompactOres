257567df2a97ea950c748e987a0b89e70bce98bc
DoubleNegation <43982164+DoubleNegation@users.noreply.github.com>
2022-05-14 16:55:46 +0200

    Greatly improve debugworldgen performance
    Achieved by skipping world.setBlock, calling chunk.setBlockState directly instead.
    The disadvantage with this solution is that sometimes ghost block chunks remain in the world.

--- src/main/java/doublenegation/mods/compactores/debug/WorldGenDebugging.java
+++ src/main/java/doublenegation/mods/compactores/debug/WorldGenDebugging.java
@@ -13,6 +13,8 @@ import net.minecraft.server.TickTask;
 import net.minecraft.server.level.ChunkHolder;
 import net.minecraft.server.level.ChunkMap;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
@@ -20,6 +22,7 @@ import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.storage.ServerLevelData;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
@@ -110,6 +113,24 @@ public class WorldGenDebugging {
                 processChunk(world, chunk);
             }
         }
+        try {
+            // respawn the player that ran the command, forcing all chunks around them to be resent,
+            // making all the ghost blocks that are now left disappear.
+            // obviously won't work for other players or when the command is run from a command block.
+            ServerPlayer player = ctx.getSource().getPlayerOrException();
+            double x = player.getX(), y = player.getY(), z = player.getZ();
+            float xRot = player.getXRot(), yRot = player.getYRot();
+            player.changeDimension(player.getLevel(), new ITeleporter() {
+                @Override public Entity placeEntity(Entity entity, ServerLevel currentWorld, ServerLevel destWorld, float yaw, Function<Boolean, Entity> repositionEntity) {
+                    Entity newEntity = ITeleporter.super.placeEntity(entity, currentWorld, destWorld, yaw, repositionEntity);
+                    newEntity.setPos(x, y, z);
+                    newEntity.setXRot(xRot);
+                    newEntity.setYRot(yRot);
+                    return newEntity;
+                }
+            });
+        } catch(Exception ignored) {
+        }
         return 0;
     }
     
@@ -141,7 +162,7 @@ public class WorldGenDebugging {
             for(int z = 0; z < 16; z++) {
                 for(int x = 0; x < 16; x++) {
                     if(!ores.contains(chunk.getBlockState(new BlockPos(x, y, z)).getBlock().getRegistryName())) {
-                        world.setBlock(new BlockPos(cx + x, y, cz + z), Blocks.AIR.defaultBlockState(), 2 | 16);
+                        chunk.setBlockState(new BlockPos(cx + x, y, cz + z), Blocks.AIR.defaultBlockState(), false);
                         count++;
                     }
                 }
