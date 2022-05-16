6db386dfb7c0d5ff17343599cf8075b860217d6a
DoubleNegation <43982164+DoubleNegation@users.noreply.github.com>
2022-05-16 21:46:36 +0200

    Small cleanups in CompactOreBlock
    Now using the static random source in the block class instead of getting one from elsewhere.
    Also updated an outdated comment.

--- src/main/java/doublenegation/mods/compactores/CompactOreBlock.java
+++ src/main/java/doublenegation/mods/compactores/CompactOreBlock.java
@@ -6,27 +6,24 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.storage.loot.LootContext;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
 public class CompactOreBlock extends Block {
 
     private final CompactOre ore;
 
     public CompactOreBlock(CompactOre ore) {
-        // all vanilla ores (except gilded blackstone) have hardness and resistance values of 3, so use 3 here too
+        // all vanilla stone and netherrack ores have hardness and resistance values of 3, so use 3 here too
+        // deepslate ones have hardness 4.5, resistance 3, but this may be very difficult to implement
         super(Properties.of(Material.STONE).sound(SoundType.STONE).requiresCorrectToolForDrops().strength(3, 3));
         this.ore = ore;
     }
@@ -42,8 +39,7 @@ public class CompactOreBlock extends Block {
 
     @Override
     public int getExpDrop(BlockState state, LevelReader world, BlockPos pos, int fortune, int silktouch) {
-        Random rand = Optional.of(world.getChunk(pos)).map(ChunkAccess::getWorldForge).map(LevelAccessor::getRandom).orElse(new Random());
-        int r = ore.getMinRolls() + rand.nextInt(ore.getMaxRolls() - ore.getMinRolls() + 1);
+        int r = ore.getMinRolls() + RANDOM.nextInt(ore.getMaxRolls() - ore.getMinRolls() + 1);
         return ore.getBaseBlock().getExpDrop(ore.getBaseBlock().defaultBlockState(), world, pos, fortune, silktouch) * r;
     }
 
@@ -77,8 +73,7 @@ public class CompactOreBlock extends Block {
         if(ore.isUseGetDrops()) {
             List<ItemStack> parentList = super.getDrops(state, builder);
             List<ItemStack> oreList = ore.getBaseBlock().getDrops(ore.getBaseBlock().defaultBlockState(), builder);
-            Random rand = builder.getLevel().getRandom();
-            int r = ore.getMinRolls() + rand.nextInt(ore.getMaxRolls() - ore.getMinRolls() + 1);
+            int r = ore.getMinRolls() + RANDOM.nextInt(ore.getMaxRolls() - ore.getMinRolls() + 1);
             for(int i = 0; i < r; i++) {
                 for(ItemStack stack : oreList) {
                     parentList.add(new ItemStack(stack.getItem(), stack.getCount(), stack.getTag()));
